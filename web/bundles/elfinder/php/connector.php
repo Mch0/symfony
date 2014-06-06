c313159b62f69f6773e77ce719421927a36ea05f
SCOTE Arnaud <arnaud.scote@netapsys.fr>
2014-06-06 15:50:31 +0200

    (fix) url pour les media en local a changer pour la prod

--- web/bundles/elfinder/php/connector.php
+++ web/bundles/elfinder/php/connector.php
@@ -31,8 +31,8 @@ $opts = array(
 	'roots' => array(
 		array(
 			'driver'        => 'LocalFileSystem',   // driver for accessing file system (REQUIRED)
-			'path'          => '../../upload',         // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '../../upload/', // URL to files (REQUIRED)
+			'path'          => '../../upload/',         // path to files (REQUIRED)
+			'URL'           => '../../../bundles/upload/', // URL to files (REQUIRED)
 			'accessControl' => 'access'             // disable and hide dot starting files (OPTIONAL)
 		)
 	)
