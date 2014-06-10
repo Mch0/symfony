1e0c271ea0218b0c10348856b5afe30b29b4dba3
SCOTE Arnaud <arnaud.scote@netapsys.fr>
2014-06-10 15:10:35 +0200

    (blog) stylisation de la page d'accueil pour article

--- src/tgw/BlogBundle/Controller/AccueilController.php
+++ src/tgw/BlogBundle/Controller/AccueilController.php
@@ -5,6 +5,7 @@ namespace tgw\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use tgw\BlogBundle\Entity\Article;
 
+
 class AccueilController extends Controller
 {
 
@@ -14,7 +15,10 @@ class AccueilController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('tgwBlogBundle:Default:index.html.twig', array('titre' => 'Accueil'));
+        $DoctrineService = $this->getDoctrine()->getManager();
+        $articles = $DoctrineService->getRepository('tgwBlogBundle:Article')->findBy(array('articlePublie' =>true),array('articleDate' => 'DESC'),5,0);
+
+        return $this->render('tgwBlogBundle:Default:index.html.twig', array('titre' => 'Accueil','articles' => $articles));
     }
 
 }
