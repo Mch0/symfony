4208f62ec0f15a709e7ed4d49c31c9e6a793f7ff
SCOTE Arnaud <arnaud.scote@netapsys.fr>
2014-06-16 17:40:00 +0200

    (blog) recherche ok

---
+++ src/tgw/BlogBundle/Controller/RechercheController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace tgw\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use tgw\BlogBundle\Entity\Article;
+
+
+class RechercheController extends Controller
+{
+
+        public function rechercherAction(Request $request)
+        {
+            $motClef = $request->get('mot-clef');
+            $em = $this->getDoctrine()->getEntityManager();
+            $query = $em->createQuery("select a FROM tgwBlogBundle:Article a WHERE a.articleContenu like :motClef
+            or a.articleTitre like :motClef")->setParameter('motClef',"%".$motClef."%");
+            $articles = $query->getResult();
+
+            return $this->render('tgwBlogBundle:Recherche:resultat.html.twig',array('articles'=>$articles,
+                'titre' =>  $this->get('translator')->trans('recherche',array('%mot-clef%'=>$motClef))));
+        }
+
+}
